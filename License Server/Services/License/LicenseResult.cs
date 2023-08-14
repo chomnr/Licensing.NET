51e0deeaaea35dfa75683ed1703f053ec1af9c0d
chomnr <zeljkovranjes@outlook.com>
2023-08-14 15:20:24 -0400

    forgot to add value to .Error.

--- License Server/Services/License/LicenseResult.cs
+++ License Server/Services/License/LicenseResult.cs
@@ -6,18 +6,19 @@ namespace License_Server.Services.Licensing
     {
         public AUTHORITY_STATE AuthorityState { get; set; }
         public License? License { get; set; }
-        public LicenseError? error { get; set; }
+        public LicenseError? Error { get; set; }
 
         public LicenseResult() { }
 
         public LicenseResult(License? license, AUTHORITY_STATE state, LicenseError? error) { 
             this.License = license;
             this.AuthorityState = state;
+            this.Error = error;
         }
 
         public LicenseResult(LicenseError error, AUTHORITY_STATE state)
         {
-            this.error = error;
+            this.Error = error;
             this.AuthorityState = state;
             if (state == AUTHORITY_STATE.REJECTED)
             {
